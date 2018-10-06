6e61fce3720a194eddacd9a0245b59769091445a
Areli Agudo <areliagudo@Arelis-MacBook-Pro.local>
2018-10-06 14:54:10 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  codePath
+//
+//  Created by Areli Agudo on 10/6/18.
+//  Copyright © 2018 Areli Agudo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
